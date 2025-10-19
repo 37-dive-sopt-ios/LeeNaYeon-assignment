213462e247ca46fd83b828ab484a744d33c08e2b
yeonee <leenayeon@sookmyung.ac.kr>
2025-10-19 15:34:11 +0900

    init: 프로젝트 기초세팅

---
+++ sopt-37th-assignment/sopt-37th-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sopt-37th-assignment
+//
+//  Created by 이나연 on 10/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
