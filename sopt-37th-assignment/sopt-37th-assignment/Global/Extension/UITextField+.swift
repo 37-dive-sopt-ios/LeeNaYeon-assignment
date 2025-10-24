269e92b4a8bfaf34400372af1bc569fb8799ddeb
yeonee <leenayeon@sookmyung.ac.kr>
2025-10-25 00:54:58 +0900

    feat: #1 placeholder 익스텐션

--- sopt-37th-assignment/sopt-37th-assignment/Global/Extension/UITextField+.swift
+++ sopt-37th-assignment/sopt-37th-assignment/Global/Extension/UITextField+.swift
@@ -24,5 +24,13 @@ extension UITextField {
         addLeftPadding(leftAmount)
         addRightPadding(rightAmount)
     }
+    
+    func setPlaceholder(color: UIColor) {
+        guard let string = self.placeholder else {
+            return
+        }
+        attributedPlaceholder = NSAttributedString(string: string, attributes: [.foregroundColor: color])
+    }
+    
 }
 
